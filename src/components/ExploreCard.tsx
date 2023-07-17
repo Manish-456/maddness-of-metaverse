459d743e31581373b6c35df55da51e26656336e1
Manish-456 <tamangmanish3456@gmail.com>
2023-07-17 20:15:34 +0545

    padding adjusted

--- src/components/ExploreCard.tsx
+++ src/components/ExploreCard.tsx
@@ -37,12 +37,12 @@ export default function ExploreCard({
         active !== world.id ? (
             <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">{world.title}</h3>
         ) : (
-            <div className="absolute bottom-0 p-8 
+            <div className="absolute bottom-0 p-4 
             z-4
            flex
             justify-start w-full flex-col bg-black/[0.5] rounded-b-[24px]">
                 <div className={`${styles.flexCenter} h-[60px] flex items-center justify-center w-[60px] rounded-[24px] glassmorphism mb-[16px]`}>
-                <Image fill src="/headset.svg" alt="headset" 
+                <Image width={50} height={50} src="/headset.svg" alt="headset" 
                 className="w-1/2 h-1/2 object-cover"/>
                 </div>
                 <p className="font-normal text-[16px] text-white uppercase leading-[20px]">Enter the Metaverse</p>
